7c3c7444c80cd2c534004e8bb617686c0128a036
0xBandito <0xbandito@0xBanditos-MBP.attlocal.net>
2022-08-04 15:07:04 -0400

    deposit weth into aave

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Deposit collateral: ETH / Wrapped Eth (WETH)
+2. Borrow other assets: DAI
+3. Repay the DAI
+
+MAINNET FORKING
+
+PROS : QUICK | EASY | MAINNET COPY
+CONS : NEED AN API, SOME CONTRACTS ARE COMPLEX
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,72 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const REPORT_GAS = process.env.REPORT_GAS || false
+
 module.exports = {
-  solidity: "0.8.9",
-};
+    solidity: {
+        compilers: [
+            {version: "0.8.8"},
+            {version: "0.6.6"},
+            {version: "0.6.12"},
+            {version: "0.4.19"},
+        ]
+    },
+    defaultNetwork: "hardhat",
+
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL
+            },
+            blockConfirmations:1
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            saveDeployments: true,
+            chainId: 4,
+        }
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0
+        },
+        player: {
+            default: 1
+        }, 
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            rinkeby: ETHERSCAN_API_KEY,
+        },
+    },
+    gasReporter: {
+        enabled: REPORT_GAS,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+    },
+    contractSizer: {
+        runOnCompile: false,
+        only: ["Lottery"],
+    },
+    mocha: {
+        timeout: 500000,
+    }
+}
